3615b3468524ca7671a847ceb562a1c0fc44aeea
Portmann Peter <peter.portmann@thalesgroup.com>
2024-10-11 12:28:21 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # jasmine-data_driven_testing
-With this plugin for Jasmine 5.x, data-driven testing is very easy.
+
+With this TypeScript plugin for Jasmine 5.x, data-driven testing is very easy.
+
+## Contents
+
+With this package you can run data-driven Jasmine tests for your TypeScript code. The output is displayed in your terminal by default.
+
+## Documentation
+
+[https://github.com/pekarasa/jasmine-data_driven_testing](https://github.com/pekarasa/jasmine-data_driven_testing#readme)
+
+## Quick Start
+
+Installation:
+
+- First install Jasmine as described at [jasmine](https://www.npmjs.com/package/jasmine).
+
+- Then jasmine-data_driven_testing is installed with:
+
+    ```sh
+    npm install --save-dev jasmine-data_driven_testing
+    ```
+
+Write your first test:
+
+```typescript
+import { all } from "jasmine-data_driven_testing";
+
+describe("PatchFunctions", function () {
+    all<any>([
+        [""],
+        [null],
+        [undefined]
+    ], "blank values are invalid", function (param) {
+        expect(value).toBe(true);
+    });
+}
+```
+
+## Support
+
+Documentation: [github.com/pekarasa/jasmine-data_driven_testing](https://github.com/pekarasa/jasmine-data_driven_testing#readme)<br>
+Please file issues here at GitHub.
+
+Copyright (c) 2024Peter Portmann
+This software is licensed under the MIT License.
---
+++ src/index.ts
@@ -0,0 +1,35 @@
+
+type TestDeclarationFunction = (expectation: string, assertion?: jasmine.ImplementationCallback, timeout?: number) => void;
+
+export function all<T1>(dataRows: [T1][], description: string, testMethod: (t1: T1) => PromiseLike<any> | void): void;
+export function all<T1, T2>(dataRows: [T1, T2][], description: string, testMethod: (t1: T1, t2: T2) => PromiseLike<any> | void): void;
+export function all<T1, T2, T3>(dataRows: [T1, T2, T3][], description: string, testMethod: (t1: T1, t2: T2, t3: T3) => PromiseLike<any> | void): void;
+export function all<T1, T2, T3, T4>(dataRows: [T1, T2, T3, T4][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4) => PromiseLike<any> | void): void;
+export function all<T1, T2, T3, T4, T5>(dataRows: [T1, T2, T3, T4, T5][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => PromiseLike<any> | void): void;
+export function all<T1, T2, T3, T4, T5, T6>(dataRows: [T1, T2, T3, T4, T5, T6][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => PromiseLike<any> | void): void;
+export function all<T1, T2, T3, T4, T5, T6, T7>(dataRows: [T1, T2, T3, T4, T5, T6, T7][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => PromiseLike<any> | void): void;
+export function all<T1, T2, T3, T4, T5, T6, T7, T8>(dataRows: [T1, T2, T3, T4, T5, T6, T7, T8][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => PromiseLike<any> | void): void;
+export function all<T1, T2, T3, T4, T5, T6, T7, T8, T9>(dataRows: [T1, T2, T3, T4, T5, T6, T7, T8, T9][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8, t9: T9) => PromiseLike<any> | void): void;
+export function all(dataRows: Array<Array<any>>, description: string, testMethod: (...args: any[]) => PromiseLike<any> | void): void {
+    allImpl(dataRows, description, testMethod, it);
+}
+
+export function fall<T1>(dataRows: [T1][], description: string, testMethod: (t1: T1) => PromiseLike<any> | void): void;
+export function fall<T1, T2>(dataRows: [T1, T2][], description: string, testMethod: (t1: T1, t2: T2) => PromiseLike<any> | void): void;
+export function fall<T1, T2, T3>(dataRows: [T1, T2, T3][], description: string, testMethod: (t1: T1, t2: T2, t3: T3) => PromiseLike<any> | void): void;
+export function fall<T1, T2, T3, T4>(dataRows: [T1, T2, T3, T4][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4) => PromiseLike<any> | void): void;
+export function fall<T1, T2, T3, T4, T5>(dataRows: [T1, T2, T3, T4, T5][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5) => PromiseLike<any> | void): void;
+export function fall<T1, T2, T3, T4, T5, T6>(dataRows: [T1, T2, T3, T4, T5, T6][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6) => PromiseLike<any> | void): void;
+export function fall<T1, T2, T3, T4, T5, T6, T7>(dataRows: [T1, T2, T3, T4, T5, T6, T7][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7) => PromiseLike<any> | void): void;
+export function fall<T1, T2, T3, T4, T5, T6, T7, T8>(dataRows: [T1, T2, T3, T4, T5, T6, T7, T8][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8) => PromiseLike<any> | void): void;
+export function fall<T1, T2, T3, T4, T5, T6, T7, T8, T9>(dataRows: [T1, T2, T3, T4, T5, T6, T7, T8, T9][], description: string, testMethod: (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8, t9: T9) => PromiseLike<any> | void): void;
+export function fall(dataRows: Array<Array<any>>, description: string, testMethod: (...args: any[]) => PromiseLike<any> | void): void {
+    allImpl(dataRows, description, testMethod, fit);
+}
+
+
+function allImpl(dataRows: Array<Array<any>>, description: string, testMethod: (...args: any[]) => PromiseLike<any> | void, testDeclarationFunction: TestDeclarationFunction): void {
+    dataRows.forEach((dataRow, index) => {
+        testDeclarationFunction(`${description} Nr. ${index + 1} (${dataRow.map(x => JSON.stringify(x)).join(", ")})`, () => testMethod(...dataRow));
+    });
+}
